5fd17ca2d22d076bfdc27d70b4aa4cd6dd3fd89c
Vlad <baraewlad@gmail.com>
2019-05-10 00:06:10 +0300

    driver

--- Python/train_driver.py
+++ Python/train_driver.py
@@ -14,9 +14,9 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     if not args.arg:
-        for i in [105, 15]:
+        for i in [105, 5, 15]:
             print('Running script with {}'.format(i))
-            sp.check_call([sys.executable, 'train1.py', '--arg', str(i)])
+            sp.check_call([sys.executable, 'train_driver.py', '--arg', str(i)])
     else:
         from train_network import *
         train_both_tasks(2, int(args.arg), two_times=True, batch_size=36, lr=0.001, epoch=180)
