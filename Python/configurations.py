d72e9ecfa92bdbf618725aec4bdbd5b85ec11e3a
Nastassya Horlava <g.nasta.work@gmail.com>
2019-07-14 14:19:31 +0300

    run for server

--- Python/configurations.py
+++ Python/configurations.py
@@ -1,20 +1,22 @@
 import numpy as np
+
 true_vector = np.array([1.0, 0.0])
 false_vector = np.array([0.0, 1.0])
-number_of_trials=110
-number_of_subjects=105
-home_repo = '../../Data/neural_network_models/'
-repo_with_raw_data = '../../Data/Result_json/'
-matlab_repo_for_saving_svm = '../../Data/NN_convoluted/nn'
+number_of_trials = 110
+number_of_subjects = 105
+general_repo = 'vsc-hard-mounts/leuven-user/329/vsc32985/'
+home_repo = general_repo + 'Data/neural_network_models/'
+repo_with_raw_data = general_repo + 'Data/Result_json/'
+matlab_repo_for_saving_svm = general_repo + 'Data/NN_convoluted/nn'
 
 earlystop = False
-data_len=220
-two_times=True
-batch_size=140
-lr=0.001
-epoch=600
+data_len = 220
+two_times = True
+batch_size = 140
+lr = 0.001
+epoch = 500
 
-list_of_subjects = [i for i in range(1,number_of_subjects+1, 1)]
+list_of_subjects = [i for i in range(1, number_of_subjects+1, 1)]
 global_task = "Task1"
 
-nn = "simple_1_with_dropout_2"
+nn = "nn_inception_1_with_small_kernel"
--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-cd Python
+cd Python/nn_models
+python nn_inception_1_with_small_kernel.py
+cd ../
 python Run_subjects.py
