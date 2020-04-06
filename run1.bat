ca4c0495227ceab675c7a0e52074d297fde0cb27
Nastassya Horlava <g.nasta.work@gmail.com>
2020-04-06 17:35:03 +0200

    fixed

--- run1.bat
+++ run1.bat
@@ -1,4 +1,4 @@
 cd Python/nn_models
 python nn_inception_1_for_16_channels.py
 cd ../
-python Run_subjects.py --arg2 inception_1_16_channels--arg3 16_channels
+python Run_subjects.py --arg2 inception_1_16_channels --arg3 16_channels
