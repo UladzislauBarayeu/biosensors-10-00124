7de0b612a643a748d6c65173549cda4486f0f8dc
HorlavaNastassya <g.nasta.work@gmail.com>
2020-09-14 20:18:56 +0200

    final version

--- matlab/Run0.m
+++ matlab/Run0.m
@@ -9,10 +9,9 @@
 folderpath='Data/input/';
 task=1;%set number of task
 min_trials=105;% min number of  trials in all subjects
-%% run only on matlab2016
-%preproces( task, folderpath, min_trials);% run only on matlab2016
+preproces( task, folderpath, min_trials);% run only on matlab2016
 
-%% this could run on matlab2020
+
 %% EMD
 type=0;% 1-emd from lena 2-emd from internet 3-NAMEMD, 0-original func
 number_of_IMFS=4;%set how many IMF left
